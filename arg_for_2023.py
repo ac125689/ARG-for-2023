c0f28fd33a37c15f5b466b29c36d6d8623b81b86
ac125689 <atirumala844@gmail.com>
2023-06-07 08:05:41 -0400

    Update arg_for_2023.py

--- arg_for_2023.py
+++ arg_for_2023.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-from stories import Stories
+from streamlit_extras.add_vertical_space import add_vertical_space
 from home import home
 def dissclamer():
     place = st.empty()
@@ -48,7 +48,48 @@ def main():
     if "Home" == selected:
         home()
     if "Stories" == selected:
-        Stories()
+        tab1, tab2,tab3,tab4,tab5,tab6,tab7,tab8,tab9,tab10 = st.tabs(['story 1','story 2', 'story 3','story 4','story 5','story 6','story 7','story 8','story 9','story 10'])
+    with tab1:
+        st.subheader("story 1")
+        st.write("Once upon a time, in the bustling halls of West Windsor-Plainsboro High School South (HSS), a new student named Raj arrived from India. Raj was an innocent and curious individual, eager to adapt to his new environment. Little did he know that his journey in this school would be filled with mystery and intrigue.")
+        add_vertical_space()
+        st.write("Walking down the hall to his first class, Raj almost bumped into someone.“Watch where you’re going dude!” the person he almost bumped into yells not unkindly.Bustling was definitely the right way to describe this place. Everyone moved about, knowing where to go and how to skillfully avoid accidents. Well, everyone except Raj. Although that probably had something to do with the fact that he was new.")
+        add_vertical_space()
+        st.write("Luckily, was starting to get the hang of the schedule. As Raj settled into his classes, he noticed two distinct personalities among his classmates. The first of these was Ram Panchangam, a highly intelligent and puzzle-loving student, who always seemed to be one step ahead in solving mysteries.")
+        add_vertical_space()
+        st.write("Raj had met Ram on his first day in his forensics class. Raj and Ram were assigned to sit next to each other. Raj shyly smiled at Ram, not sure how to approach a new person.")
+        add_vertical_space()
+        st.write("“Hey, I’ve never seen you around before. I’m Ram, what’s your name? Why did you take this class? I want to be a detective so that’s why I’m here, you know cause it would totally help me I’m sure!”")
+        add_vertical_space()
+        st.write("Well, it seemed as though Ram was the opposite, greeting Raj excitedly as though he wasn’t a complete stranger.")
+        add_vertical_space()
+        st.write("“Uh, I’m Raj. I just took it because I thought it would be cool. And um, good for you, the uh, detective stuff.” Raj responded, voice wavering a bit.")
+        add_vertical_space()
+        st.write("“It’s great to meet you Raj! I guess we’ll be partners for this marking period!” Ram responded enthusiastically.")
+        add_vertical_space()
+        st.write("Throughout the next few weeks, Raj and Ram got to learn about each other, like their strengths and weaknesses. Raj got more comfortable with Ram and opened up, earning each other as friends.")
+        add_vertical_space()
+        st.write("On the other hand, Raj met Trishna Shyamala, who had a malicious nature and took pleasure in causing problems for others, including Raj.")
+        add_vertical_space()
+        st.write("Raj had met Trishna on his second day, during gym class. Raj had always had good intuition so he knew something was off with Trishna. After his meeting with Ram, Raj’s confidence had improved a bit. So, he felt confident enough to introduce himself to the boy with a gym locker next to his before class started.")
+        add_vertical_space()
+        st.write("“Hi, I’m Raj. I’m new here,” Raj said, extending his hand.")
+        add_vertical_space()
+        st.write("“And I don’t care, newbie,” Trishna replied arrogantly, walking away, leaving a stunned Raj behind.")
+        add_vertical_space()
+        st.write("After that, Raj decided it might be best to stay away from Trishna. Unfortunately, that simply didn’t seem to be an option for him…")
+        add_vertical_space()
+        st.write("One day, Raj stumbled upon a peculiar painting in the school hallway. The painting depicted a compass pointing southeast, captivating his attention. Below the compass, a riddle was inscribed:")
+        add_vertical_space()
+        st.write("""In  🏫 ,  🅰 painting with compass bold , 
+Points southeast ,  🅰 tale to unfold . 
+Look below , where pirates roam , 
+How many good  👀 call it home ? 
+
+A pirate awaits ,  🅰 daring sight , 
+But how many  👀 shine with light ? 
+Seek the answer , let curiosity guide , 
+The painting ' s  ㊙ it shall confide . """)
 
 
 
