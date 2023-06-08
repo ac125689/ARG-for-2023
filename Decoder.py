890bbb9537bfb37baf537d72d9d61a1643e1d6cc
ac125689 <atirumala844@gmail.com>
2023-06-08 14:11:47 -0400

    Update Decoder.py

--- Decoder.py
+++ Decoder.py
@@ -2,5 +2,8 @@ import streamlit as st
 from encoder_and_decoder import encode_binary,encode_to_hex,encode_Unicode,url_encoder,base64_encode,morse_code_encoder,decode_binary,decode_from_hex,decode_Unicode,url_decoder,base64_decode,morse_code_decoder
 
 def decoder():
-    sele = st.text_area('past the code here')
-    st.write(sele)
+    mess = st.text_area('Past the riddle here')
+    sele = st.selectbox('Select wich type do you want?',['Binary encoder','Hex encoder','Unicode encoder','Url encoder','Base64 encoder','Morse code encoder','Binary decoder','Hex decoder','Unicode decoder','Url decoder','Base64 decoder','Morse code decoder'])
+    if sele == 'Binary encoder':
+        with st.button('Encode to binary'):
+            st.success(encode_binary(mess))
